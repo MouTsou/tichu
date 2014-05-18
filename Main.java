28b8373d48e778f3e31be6d919d2e8613a78bbb1
teris1994 <leuterisasp@gmail.com>
2014-05-18 20:17:30 +0300

    Create main.java
    -Add Variables 
    -Add Number Card
    -Add Specail Cards

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main
+{
+
+  public class main(String[] , args)
+  {
+    //1 = A
+    //11= J
+    //12= Q
+    //13= K
+    
+    int[] numbers = { "1","2","3","4","5","6","7","8","9","10","11","12,"13"};
+    
+    //14 = Dogs
+    //15 = Majon
+    //16 = Phoenix
+    //17 = Dragon
+    
+    int[] special_cards={"14","15","16","17"};
+    
+    
+    
+  }
+
+
+}
